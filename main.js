b5ec4efef47fbc6d0d93856107df57efe183abb1
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2021-12-11 08:21:34 +0700

    bind()

--- main.js
+++ main.js
@@ -125,3 +125,56 @@ const app=(function(){
 
     }
 })()
+
+//bind()
+console.log('Bind');
+const $=document.querySelector.bind(document);
+const $$=document.querySelectorAll.bind(document);
+const appp =(()=>{
+    const cars=['BMW'];
+    const input=$('#input');
+    const root=$('#root');
+    const submit=$('#submit');
+    return {
+        add(car){
+            cars.push(car);
+        },
+        delete(index){
+            cars.splice(index,1);
+        },
+        render(){
+            const html=cars.map((car,index)=>`
+                <li>
+                    ${car}
+                    <span class="delete" data-index="${index}">&times</span>
+                </li>
+            `)
+            .join('')
+            root.innerHTML = html;
+        },
+        handleDelete(e){
+            // console.log(e.target);
+            const deleteBtn=e.target.closest('.delete');
+            if(deleteBtn){
+                const index=deleteBtn.dataset.index;
+                // console.log(index);
+                this.delete(index);
+                this.render();
+            }
+        },
+        init(){
+            //hande DOM event
+            submit.onclick=()=>{
+                const car=input.value
+                this.add(car)
+                this.render();
+                input.value=null;
+                input.focus()
+            }
+            root.onclick= this.handleDelete.bind(this);
+            this.render();
+        }      
+    }
+})();
+appp.init()
+//Delicate
